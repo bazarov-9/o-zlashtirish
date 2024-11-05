f4e0d96cd8edacc09d7ed0eedb5f73301daf08a6
bazarov-07 <bonurbazarov07@gmail.com>
2024-11-05 14:47:33 +0500

    o'zgardi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}", 
+    "./**/*.html"           
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
